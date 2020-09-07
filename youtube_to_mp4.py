1f075b877d6136e854a78673e1177c86e6f5d8f1
AstroCoding <fantaboy33@gmail.com>
2020-09-07 14:05:46 -0400

    Added download progress bar; Forced mp4 with highest resolution

--- youtube_to_mp4.py
+++ youtube_to_mp4.py
@@ -1,11 +1,7 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 from datetime import datetime
-
-# Get Link from User
-link = str(input("Please paste the link to the video you would like to download: \n"))
-
-# Get video object
-reqVideo = YouTube(link)
+import os
 
 # Following are the properties (information about the YouTube video) of the YouTube object which we can access.
 # 1. Title
@@ -17,21 +13,29 @@ reqVideo = YouTube(link)
 # 7. Age Restricted
 # 8. Video Id
 
-# Get Confirmation on Video
-response = str(input(f"Please Confirm (y/n) - {reqVideo.title} ({reqVideo.length}): "))
+# Get Link from User
+link = str(input("Please paste the link to the video you would like to download: \n"))
 
-while response != 'y' and response != 'n':
-    response = str(input(f"Sorry, Please Confirm (y/n) - {reqVideo.title} ({reqVideo.length}sec): "))
+# Get video object
+try:
+    reqVideo = YouTube(link, on_progress_callback=on_progress)
+    # Get Confirmation on Video
+    response = str(
+        input(f"Please Confirm (y/n) - {reqVideo.title} ({reqVideo.length}sec): "))
 
-# Depending on Confirmation, preform actions and exit.
-if response == 'y':
-    print("Video Confirmed. Downloading...")
-    date = datetime.today().strftime('%Y-%m-%d')
-    stream = reqVideo.streams.first()
-    stream.download(f'./videos/{date}/')
-    print('Finished. Exiting...')
-    exit(0)
-else:
-    print("Wrong video input, exiting...")
-    exit(0)
+    while response != 'y' and response != 'n':
+        response = str(input(
+            f"Sorry, Please Confirm (y/n) - {reqVideo.title} ({reqVideo.length}sec): "))
 
+    # Depending on Confirmation, preform actions and exit.
+    if response == 'y':
+        print("Video Confirmed. Downloading...")
+        date = datetime.today().strftime('%Y-%m-%d')
+        reqVideo.streams.filter(file_extension='mp4').get_highest_resolution().download(f'{os.path.curdir}/videos/{date}/')
+        print('\nFinished. Exiting...\n')
+        exit(0)
+    else:
+        print("Wrong video input, exiting...")
+        exit(0)
+except EOFError as err:
+    print(err)
